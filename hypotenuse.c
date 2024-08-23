b5beeefda5dff6d3232b7bb7ed9a08a6b2f602aa
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2024-08-23 09:56:09 +0100

    added switch cases and if statements to my bag

--- hypotenuse.c
+++ hypotenuse.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
   float a;
   float b;
   printf("\nWhat is the side a?");
   scanf("\n%f", &a);
   printf("\nWhat is the side b?");
-  scanf("\n%f",&b);
-  float c = sqrt(pow(a,2)+pow(b,2));
-  printf("\nThe hypotenuse of this tringle is: %f",c);
+  scanf("\n%f", &b);
+  float c = sqrt(pow(a, 2) + pow(b, 2));
+  printf("\nThe hypotenuse of this tringle is: %f", c);
   return EXIT_SUCCESS;
 }
